81c6a89e4619fddb990f68b8bd976db03fa757de
tonyskulk <tony.skulk@gmail.com>
2023-06-28 06:48:21 +0000

    changed size

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -3,7 +3,7 @@
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
-      style="width: 400px; height: 400px"
+      style="width: 200px; height: 200px"
     >
   </q-page>
 </template>
